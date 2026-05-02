620ec076183c544cb4f560e53bb27319152109f6
Marc Baque <marc.baque@nubos.cloud>
2026-05-02 19:31:40 +0200

    chore: add Vite configuration file for staged checks

--- app/pages/sign-in.vue
+++ app/pages/sign-in.vue
@@ -1,3 +1,16 @@
 <template>
-  <NuxtLink to="/">To Index</NuxtLink>
+  <div class="grid h-screen place-items-center">
+    <UCard
+      title="Sign in here"
+      variant="soft"
+    >
+      <NuxtLink to="/">To Index</NuxtLink>
+      <template #footer>
+        <UButton
+          label="Sign in"
+          class="w-full justify-center"
+        />
+      </template>
+    </UCard>
+  </div>
 </template>
