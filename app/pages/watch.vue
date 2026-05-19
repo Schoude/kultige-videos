72ba2bf77f72773c5b16d88378760f5fc298d78c
Marc Baque <marc.baque@nubos.cloud>
2026-05-19 21:41:03 +0200

    feat: add watch page for video playback and remove videojs dependency
    - Implemented a new watch page in `watch.vue` for displaying video content.
    - Added logic to fetch video data from Supabase based on the video ID from the route.
    - Removed the `@videojs/html` dependency from `package.json` and updated `pnpm-lock.yaml` accordingly.

---
+++ app/pages/watch.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      type: 'module',
+      src: 'https://cdn.jsdelivr.net/npm/@videojs/html/cdn/video.js',
+    },
+  ],
+});
+
+const router = useRouter();
+const route = useRoute();
+const vId = route.query.v;
+
+if (vId == null || vId === '') {
+  void router.push('/');
+}
+
+const client = useSupabaseClient();
+
+const { data: video } = await useAsyncData(`video-${vId}`, async (_, { signal }) => {
+  if (vId == null) {
+    return null;
+  }
+
+  const { data: videoData } = await client
+    .from('videos')
+    .select('*')
+    .eq('url_id', vId as string)
+    .abortSignal(signal)
+    .single();
+  const {
+    data: { publicUrl },
+  } = client.storage.from('videos').getPublicUrl(`${videoData?.url_id}/video.mp4`);
+
+  return {
+    url: publicUrl,
+    ...videoData,
+  };
+});
+</script>
+
+<template>
+  <section>
+    <video-player>
+      <video-skin class="w-[66%]">
+        <video
+          class="aspect-video"
+          :src="video!.url"
+          autoplay
+        />
+      </video-skin>
+    </video-player>
+
+    <h1 class="my-2 text-xl font-semibold">
+      {{ video!.title }}
+    </h1>
+
+    <p class="text-sm">
+      {{ video!.view_count }} Aufrufe&nbsp;&nbsp;am {{ new Date(video!.created_at!).toLocaleDateString() }}
+    </p>
+    <p class="bg-muted mt-2 rounded-md p-2 text-sm">
+      {{ video!.description }}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ullam.
+    </p>
+  </section>
+</template>
